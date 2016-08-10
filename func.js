f4814d01b39e76c1f0c2d2760974d927a4e7d88e
gasin <gasinsato@gmail.com>
2016-08-11 02:55:20 +0900

    update

--- func.js
+++ func.js
@@ -5,5 +5,5 @@ function button(){
 	bodyElement.style.backgroundColor = "#"+rand[0].toString(16)+rand[1].toString(16)+rand[2].toString(16);
 	
 	var buttonElement = document.getElementById("button0");
-	buttonElement.style.fontSize = String((Math.floor(Math.random()*100+1))*10)+"%";
+	buttonElement.style.fontSize = String((Math.floor(Math.random()*20+5))*10)+"%";
 }
