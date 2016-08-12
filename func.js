01ebb1628008f4e2de8ee5754d061770e68b124b
gasin <gasinsato@gmail.com>
2016-08-12 14:37:51 +0900

    update

--- func.js
+++ func.js
@@ -1,11 +1,12 @@
 function button(){
+	/*
 	var bodyElement = document.body;
 	var rand = [];
 	for(var i = 0; i < 3; i++) rand.push(Math.floor(Math.random()*256));
 	bodyElement.style.backgroundColor = "#"+rand[0].toString(16)+rand[1].toString(16)+rand[2].toString(16);
-	
+	*/
 	var buttonElement = document.getElementById("button0");
-	buttonElement.style.fontSize = String((Math.floor(Math.random()*20+5))*10)+"%";
+	buttonElement.style.fontSize = String((Math.floor(Math.random()*20+10))*10)+"%";
 }
 
 function drawCircle() {
