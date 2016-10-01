011297b28e26d20ab62190a802ce4756ace9b81e
gasin <gasinsato@gmail.com>
2016-10-02 00:19:59 +0900

    update

--- func.js
+++ func.js
@@ -1,9 +1,25 @@
+var isPC;
+
+function init(){
+    var ua = navigator.userAgent;
+    if(ua.indexOf('iPhone')>0||ua.indexOf('iPod')>0||ua.indexOf('Android')>0||ua.indexOf('Mobile')>0||ua.indexOf('iPad')>0){
+        document.write('<link rel="stylesheet" type="text/css" href="tablet.css">');
+        isPC = 0;
+    } else{
+        document.write('<link rel="stylesheet" type="text/css" href="style.css">');
+        isPC = 1;
+    }
+    document.close();
+}
+
 function disappear(){
+    if(isPC == 0) return;
     document.getElementById("gimmick").style.display="none";
     document.getElementById("introduce").style.display="none";
 }
 
 function appear(){
+    if(isPC == 0) return;
     document.getElementById("gimmick").style.display="block";
     document.getElementById("introduce").style.display="block";
 }
